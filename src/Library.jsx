4d42cbe0d0b205753a0022f1f3515a12576e0f70
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2023-11-26 13:41:28 -0800

    nov26.1

--- src/Library.jsx
+++ src/Library.jsx
@@ -1,10 +1,53 @@
-
+import { useState } from "react"
 
 export default function Library(){
     
+const [ open, setOpen ] = useState('')
+const [ textShade, setTextShade ] = useState('seagreen')
+const handleClick = function() {
+    open === '' ? setOpen(<EssayView/>) : setOpen('')
+    textShade === 'seagreen' ? setTextShade('orange') : setTextShade('seagreen')
+    
+} 
+    
+    return(
+        <div style={{
+            display: 'flex'
+        }}>
+        <div onClick={handleClick} style={{
+            color: textShade
+        }}>
+        essays
+        </div>
+
+        <div>
+        {open}
+        </div>
+        </div>
+      
+    )
+}
+
+function EssayView(){
+    return(
+        <div style={{
+            position: 'absolute',
+            left: 150,
+            top: 200,
+            height: 720,
+            width: 790,
+            border: '1px seagreen dotted',
+            backgroundColor: '#242429'
+        }}>
+            <ListEssays/>
+        </div>
+    )
+}
+
+function ListEssays(){
     return(
         <div>
-        essays
+            essays
         </div>
     )
 }
