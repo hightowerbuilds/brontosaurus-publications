0f5e130c56040f5085768ee3f13c300c049acdba
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2023-11-28 20:39:20 -0800

    nov28.2

--- src/Essays.jsx
+++ src/Essays.jsx
@@ -46,8 +46,88 @@ function EssayView(){
 
 function ListEssays(){
     return(
-        <div>
-            essays
+        <div style={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            justifyContent: 'space-around',
+            padding: 2,
+            
+            backgroundColor: '#242429'
+        }}>
+           <div style={{
+             border: '1px seagreen dotted',
+             backgroundColor: '#242429'
+           }}>
+            ______history______
+           </div>
+           <div style={{
+             border: '1px seagreen dotted',
+             backgroundColor: '#242429'
+           }}>
+            ______technology______
+           </div>
+           <div style={{
+             border: '1px seagreen dotted',
+             backgroundColor: '#242429'
+           }}>
+            ______philosophy______
+           </div>
+           <div style={{
+             border: '1px seagreen dotted',
+             backgroundColor: '#242429'
+           }}>
+            ______art______
+           </div>
+          <div>
+          <div style={{
+            height: 300,
+            width: 172,
+            position: 'absolute',
+            left: 10,
+            top: 27,
+            border: '1px black dotted',
+            borderRadius: 3
+
+           }}>
+            ...next component goes here.
+           </div>
+           <div style={{
+            height: 300,
+            width: 200,
+            position: 'absolute',
+            left: 202,
+            top: 27,
+            border: '1px black dotted',
+            borderRadius: 3
+
+           }}>
+            first menu
+           </div>
+           <div style={{
+            height: 300,
+            width: 198,
+            position: 'absolute',
+            left: 422,
+            top: 27,
+            border: '1px black dotted',
+            borderRadius: 3
+
+           }}>
+            first menu
+           </div>
+           <div style={{
+            height: 300,
+            width: 132,
+            position: 'absolute',
+            left: 638,
+            top: 27,
+            border: '1px black dotted',
+            borderRadius: 3
+
+           }}>
+            first menu
+           </div>
+          </div>
         </div>
     )
 }
