7f25c36f463495828b573a0ea3319e6ad79b7933
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-01-16 21:01:32 -0800

    jan16.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import './App.css'
 import Banner from './components/Banner'
 
@@ -10,12 +9,7 @@ function App() {
     
      <Banner/>
 
- 
-   
     </div>
-    
-
-  
   )
 }
 
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,38 +1,16 @@
 import DinoCity from '../images/cityBronto.jpg'
 import BrontoHeader from '../images/bronto-header.jpg'
+import './Banner.css'
+
 
 export default function Banner(){
     return (
-        <div style={{
-            display: 'flex',
-            position: 'absolute',
-            left: 150,
-            height: 190,
-            width: 790,
-            marginTop: 12,
-            backgroundColor: 'black',
-            border: '1px seagreen dotted'
-          }}>
+        <div className='outerDiv'>
             
           < img src={BrontoHeader} />
-            <div style={{
-                height: 190,
-                width: 250,
-                color: 'seagreen',
-                backgroundColor: 'black',
-                fontFamily: 'monospace',
-              }}> 
-              <img src={DinoCity} style={{
-                height: 140,
-                width: 230,
-                marginTop: 15,
-                marginLeft: 10
-    
-              }} />
-              <p style={{
-                paddingLeft: 10,
-                backgroundColor: 'black'
-              }}>"Probably, probably," - Many</p>
+            <div className='innerDiv'> 
+              <img  className='imageDino' src={DinoCity} />
+              <p className='quote'>"Probably, probably," - Many</p>
                </div>
           </div>
 
