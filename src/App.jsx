76f0e36d0553b017a2c8d0795567da7b3fecc756
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-01-16 20:46:00 -0800

    jan16.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import Menu from './Menu'
-import Essays from './Essays'
-import Music from './Music'
-import Time from './Time'
-import DinoCity from './images/cityBronto.jpg'
-import BrontoHeader from './images/bronto-header.jpg'
+
 import './App.css'
-import HeadlineArticle from './HeadlineArticle'
-import Gibberish from './Gibberish'
 import Banner from './components/Banner'
 
 
@@ -16,34 +9,7 @@ function App() {
     <div>
     
      <Banner/>
-      
-    <div style={{
-      height: 820,
-      width: 140,
-      fontFamily: 'monospace',
-      fontSize: 16,
-      color: 'seagreen',
-      // borderRight: '1px seagreen dotted'
-  }}>
- 
-    <div style={{
-      marginLeft: 35,
-      marginTop: 20,
-      borderLeft: '3px skyblue dotted',
-      padding: 10
-    }}>
-     
-    <Menu />
-    <Music />
-    <Essays />
-    <Time />
-    
-   
-    </div>
-  
 
-  
-    </div>
  
    
     </div>
