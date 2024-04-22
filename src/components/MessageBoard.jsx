48bf3349aa27e2f3ba79e4a3627b455f9faa622a
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-04-21 22:20:45 -0700

    april 21.10

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,67 +1,50 @@
-import { useState } from "react"
+import { useState, useEffect } from 'react';
+// import { supabase } from '../services/supabase';
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://winxgwbznakeweruqxyd.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indpbnhnd2J6bmFrZXdlcnVxeHlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM3NDQxNjAsImV4cCI6MjAyOTMyMDE2MH0.DifHRS0YwZB3gs2MToQPp4l-mk4ev3ymoIdGMRqEqo4'
+
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 
-export default function MessageBoard() {
 
-const [ message, setMessage ] = useState('')
-const handleTextInput = (event) => {
-    setMessage(event.target.value)
-}
+function MessageBoard() {
+  const [messages, setMessages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-const [board, setBoard] =  useState('')
-const handleSetBoard = () => {
-    const duffle = [];
-    duffle.push(message)
-   setBoard(duffle) 
-    console.log('working maybe')
-    setMessage('')
-}
+  useEffect(() => {
+    const fetchData = async () => {
+     
+const { data, error } = await supabase
+.from('message_board')
+.select()
+console.log(data, error)
+setMessages(data[0])
+    };
 
+    fetchData();
+  }, []);
 
-    return (
-    <div>
-
-        {/* MESSAGE ANMD COMMENT POSTING */}
-      <div style={{
-        display: 'inline-flex'
-      }}>
-          <div style={{
-                width: '30%',
-                border: '2px seagreen solid',
-                margin: '2%',
-                display: 'flex',
-                flexWrap: 'wrap',
-                justifyContent: 'center'
-              }}>
-                <textarea style={{
-                  height: '30vh',
-                  width: '100%',
-                 padding: '5%'
-                }} 
-                type="text" 
-                placeholder="INPUT TEXT HERE"
-                value={message}
-                onChange={handleTextInput}/>
-              <button style={{
-                width: '100%,',
-                height: '2vh',
-                fontFamily: 'monospace'
-              }}
-              onClick={handleSetBoard}>
-               BUTTON
-              </button>
-          </div>
-
-          <div style={{
-                width: '60%',
-                border: '2px seagreen solid',
-                margin: '2%'
-              }}>
-            {message}
-            {board}
-          </div>
-      </div>
-
+  return (
+    <div style={{
+      height: '100vh',
+      width: '100%'
+    }}>
+      <p>
+      {messages.name} 
+      <br />
+    {messages.id}
+    <br />
+    {messages.post}
+    <br />
+    {messages.views}
+    <br />
+    {messages.replies}
+      </p>
+    
     </div>
-  )
+  );
 }
+
+export default MessageBoard;
