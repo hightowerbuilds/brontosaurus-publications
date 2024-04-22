298942d92d23b7851e6380ff727e92b6002e5c8d
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-04-21 22:51:04 -0700

    april 21.11

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 // import { supabase } from '../services/supabase';
 import { createClient } from '@supabase/supabase-js'
+import PostCard from './PostCard';
 const supabaseUrl = 'https://winxgwbznakeweruqxyd.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indpbnhnd2J6bmFrZXdlcnVxeHlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM3NDQxNjAsImV4cCI6MjAyOTMyMDE2MH0.DifHRS0YwZB3gs2MToQPp4l-mk4ev3ymoIdGMRqEqo4'
 
@@ -10,17 +11,21 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 function MessageBoard() {
   const [messages, setMessages] = useState([]);
+  const [moreMessages, setMoreMessages] = useState([])
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
-     
+    setIsLoading(true)
 const { data, error } = await supabase
 .from('message_board')
 .select()
-console.log(data, error)
+console.log(data[0], data[1], error)
 setMessages(data[0])
+setMoreMessages(data[1])
+setIsLoading(false)
+setError(error)
     };
 
     fetchData();
@@ -31,18 +36,22 @@ setMessages(data[0])
       height: '100vh',
       width: '100%'
     }}>
-      <p>
-      {messages.name} 
-      <br />
-    {messages.id}
-    <br />
-    {messages.post}
-    <br />
-    {messages.views}
-    <br />
-    {messages.replies}
-      </p>
-    
+     {isLoading && <p>it is still loading</p>}
+     {error && <p>there has been an error</p>}
+   
+    <PostCard 
+    name={messages.name}
+    created={messages.created_at}
+    post={messages.post}
+    views={messages.views}
+    replies={messages.replies}  />
+
+<PostCard 
+    name={moreMessages.name}
+    created={moreMessages.created_at}
+    post={moreMessages.post}
+    views={moreMessages.views}
+    replies={moreMessages.replies}  />
     </div>
   );
 }
---
+++ src/components/PostCard.jsx
@@ -0,0 +1,24 @@
+
+export default function PostCard(props) {
+  return (
+    <div>
+        <p style={{
+            border: '2px seagreen solid',
+            fontFamily: 'monospace',
+            padding: '2%'
+        }}>
+           NAME: {props.name} 
+            <br />
+           TIME OF POST: {props.created}
+            <br />
+            <br />
+           POST: {props.post}
+            <br />
+            <br />
+           NUMBER OF VIEWS: {props.views}
+            <br />
+           NUMBER OF REPLIES: {props.replies}
+        </p>
+    </div>
+  )
+}
