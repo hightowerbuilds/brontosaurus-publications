877e50903dab0b6424854d6d6454d2844153fb70
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-05-05 07:35:22 -0700

    may 5.1

--- src/pages/Zines.jsx
+++ src/pages/Zines.jsx
@@ -1,6 +1,21 @@
+import ZinesBanner from '../images/Zines.png'
+
 
 export default function Zines() {
   return (
-    <div>Zines</div>
+    <div>
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        height: '40vh',
+        width: '100%',
+        border: '1px seagreen solid'
+      }}>
+        <img src={ZinesBanner}/>
+      </div>
+
+
+
+    </div>
   )
 }
