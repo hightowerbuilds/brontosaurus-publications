2cd67b666e343edf71f3aa5810c00071176a5009
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-05-12 15:14:47 -0700

    may 12.4

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -11,96 +11,26 @@ export default function Dashboard() {
   return (
 
     <div className="outerContainer"> 
-   
-      <div 
-      className="innerContainer"
-      style={{
-        display: 'inline-flex',
-        width: '100%',
-        borderBottom: '2px seagreen dotted',
-        backgroundColor: '#47045844',
-        borderRadius: 1
-      }}>
-        <p 
-        className="headingP"
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontFamily: 'monospace',
-          fontSize: 30,
-          margin: 10
-        }}>
+      <div className="innerContainer">
+        <p className="headingP">
           <img 
           className="headingImg"
-          style={{
-            border: '1px seagreen solid',
-            borderRadius: 2,
-          }} 
           src={BrontoHeader}/>
         </p>  
-        <p 
-        className="navP"
-        style={{
-        display: 'inline-flex',
-        width: '100%',
-        borderLeft: '2px seagreen dotted',
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}>
-        <NavLink 
-        className="galleryNavLink"
-        to='/gallery' 
-        style={{
-            margin: '5%',
-            fontFamily: 'monospace',
-            fontSize: 24,
-            color: 'seagreen'
-        }}>Art Gallery</NavLink>
-        <NavLink 
-        className="zineNavLink"
-        to='/zines' 
-        style={{
-            margin: '5%',
-            fontFamily: 'monospace',
-            fontSize: 34,
-            color: 'seagreen'
-        }}> Zines </NavLink>
-        <NavLink 
-        className="storeNavLink"
-        to='/store' 
-        style={{
-            margin: '5%',
-            fontFamily: 'monospace',
-            fontSize: 34,
-            color: 'seagreen'
-        }}>Gift Shop</NavLink>
-        <NavLink
-        className="gamesNavLink" 
-        to='/games' 
-        style={{
-            margin: '5%',
-            fontFamily: 'monospace',
-            fontSize: 34,
-            color: 'seagreen'
-        }}>Games</NavLink>
-        
+
+        <p className="navP">
+
+        <NavLink className="galleryNavLink" to='/gallery'>art</NavLink>
+        <NavLink className="zineNavLink" to='/zines'> zines </NavLink>
+        <NavLink className="storeNavLink" to='/store'>if shop</NavLink>
+        <NavLink className="gamesNavLink" to='/games' >games </NavLink>
+
         </p>
       
       </div>   
 
-      <section 
-      className="mainSection"
-      style={{
-        display: 'flex',
-        justifyContent: 'center'
-      }}>
-      <img
-        className="mainImg"
-        style={{
-          padding: 10,
-        }}
-        src={CityBronto}  />
+      <section className="mainSection">
+        <img className="mainImg" src={CityBronto}/>
       </section>
        
         <MessageBoard />
