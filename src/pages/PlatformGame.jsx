a8305b8289f03887bb19e14dfea77d690cdb1296
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-05-18 07:31:24 -0700

    may 18.1

--- src/pages/PlatformGame.jsx
+++ src/pages/PlatformGame.jsx
@@ -21,21 +21,28 @@ useEffect(() => {
       y: 1
     }
 
+    let size = {
+      height: 100
+    }
+
     function draw() {
       context.fillStyle = 'black';
       context.fillRect( 0, 0, width, height)
       context.fillStyle = 'seagreen';
-      context.fillRect(position.x, position.y, 100, 10)
-      position.x += velocity.x
-      velocity.x += 0.1
-      position.y += velocity.y
-      velocity.y += 0.4
+      context.fillRect(position.x, position.y, 100, size.height)
+
+      if (position.y + size.height + velocity.y < canvas.height) {
+        position.x += velocity.x
+        velocity.x += 0.0
+        position.y += velocity.y
+        velocity.y += 0.4
+      } else {
+        velocity.y = 0;
+      }
+
     }
+     
 
-    
-
-
-  
     function animate(){
         window.requestAnimationFrame(animate)
         draw()
