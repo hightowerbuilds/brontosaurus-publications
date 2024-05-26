41670180575e13594ac782a02597a658c143195c
Luke Hightower <111666694+hightowerbuilds@users.noreply.github.com>
2024-05-26 12:01:05 -0700

    may 26.3

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -5,15 +5,20 @@ import './Articles.css'
 
 export default function Articles() {
   return (
-    <div>
+    <div style={{backgroundColor: 'oldlace'}}>
 
         <h2 className='articlesHeading'>articles</h2>
         <div className='pageConatiner'>
             <div className='sidebarContainer'>
-                sidebar
+                <p className='sidebarContent'>
+                    sidebar content
+                </p>
             </div>
             <div className='articleContainer'>
-                    thoughts and ideas
+                   <h2 className='articleTitle'>articles title</h2>
+                   <p className='articleContent'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia earum perferendis illo vitae sunt? Temporibus, vel qui saepe nemo maxime quidem? Doloribus iure consectetur error pariatur, fugit quisquam reiciendis perferendis.
+                   </p>
             </div>
         </div>
         
